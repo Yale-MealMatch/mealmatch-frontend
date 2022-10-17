e9ce173f405d14c902a165617c9bb64e1afed597
Braden Wong <bmw02002turbo@gmail.com>
2022-10-17 16:08:10 -0400

    Feat: Login and AuthCard

---
+++ src/routes/login/AuthCard.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Auth from 'supabase-ui-svelte';
+	// import { supabase } from 'src/utils/supabase';
+	import { createClient } from '@supabase/supabase-js'
+
+	const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+	const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+	// get keys via the settings page at https://app.supabase.io
+	const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
+</script>
+
+<Auth {supabase} providers={['google']} view="magic_link" />
