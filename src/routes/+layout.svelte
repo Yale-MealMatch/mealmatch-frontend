a09af46d1a445893c4f7dd55fda6799507881455
Braden Wong <bmw02002turbo@gmail.com>
2022-10-17 23:16:38 -0400

    Feat: Layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,18 @@
 <script>
+	import Footer from '$lib/Footer.svelte';
+	import Header from '$lib/Header.svelte';
 	import '../app.postcss';
 </script>
 
-<slot />
+<!-- TODO: Updat eHead -->
+<svelte:head>
+	<title>Yale MealMatch</title>
+	<meta name="description" content="Find Yalies to grab a meal with!" />
+	<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+	<link rel="icon" href="/favicon.ico" />
+</svelte:head>
+<Header />
+<main class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+	<slot />
+</main>
+<Footer />
