cd7867bb8f2c2869671f648dcc1bce8d71da40a2
Braden Wong <bmw02002turbo@gmail.com>
2022-10-17 23:46:36 -0400

    Feat: Sign in account

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -13,12 +13,12 @@
  -->
   <div class="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-md">
-      <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
+      <Logo class="mx-auto h-12 w-auto"/>
       <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
-      <p class="mt-2 text-center text-sm text-gray-600">
+      <!-- <p class="mt-2 text-center text-sm text-gray-600">
         Or {' '}
         <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">start your 14-day free trial</a>
-      </p>
+      </p> -->
     </div>
 
     <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
