58ab05540247363688ec1aa5c6885762b05cc568
Braden Wong <bmw02002turbo@gmail.com>
2022-10-18 11:35:11 -0400

    Feat: Form Page Init

---
+++ src/routes/form/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import {supabase} from "$lib/supabaseClient";
+	import {onMount} from "svelte";
+
+
+	// Get current logged in user
+	let user;
+	onMount(async () => {
+		user = await supabase.auth.getUser();
+	})
+</script>
