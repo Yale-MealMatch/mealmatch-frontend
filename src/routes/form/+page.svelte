fd5364755ac54e2afb5b709d6e672fc79b5e0bd4
Braden Wong <bmw02002turbo@gmail.com>
2022-10-18 11:43:40 -0400

    Feat: Functions

--- src/routes/form/+page.svelte
+++ src/routes/form/+page.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
-	import {supabase} from "$lib/supabaseClient";
-	import {onMount} from "svelte";
+	import { supabase } from '$lib/supabaseClient';
+	import { onMount } from 'svelte';
 
-
-	// Get current logged in user
-	let user;
-	onMount(async () => {
-		user = await supabase.auth.getUser();
-	})
+	let currentQuestionIndex = 0;
+	const previousSlide = () => {
+		if (currentQuestionIndex > 0) currentQuestionIndex--;
+	};
+	const nextSlide = () => {
+		if (currentQuestionIndex < questions.length - 1) currentQuestionIndex++;
+	};
+	const jumpSlide = (index: number) => (currentQuestionIndex = index);
+	$: question = questions[currentQuestionIndex] as Question;
 </script>
