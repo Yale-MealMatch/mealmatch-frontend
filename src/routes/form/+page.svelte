868e5b82ab3a8245399cd9fc0e134a3c6aa78dd5
Braden Wong <bmw02002turbo@gmail.com>
2022-10-18 18:58:27 -0400

    Fix: Remove second main

--- src/routes/form/+page.svelte
+++ src/routes/form/+page.svelte
@@ -17,7 +17,6 @@
 	};
 	const jumpSlide = (index: number) => (currentQuestionIndex = index);
 	$: question = questions[currentQuestionIndex] as Question;
-	
 
 	const queryClient = useQueryClient();
 
@@ -30,7 +29,7 @@
 		if (error) throw new Error(error.message);
 		console.log(data);
 	};
-	
+
 	const postUserData = async (data: any) => {
 		const { error } = await supabase
 			.from('responses')
@@ -47,36 +46,33 @@
 			queryClient.invalidateQueries('userData');
 		}
 	});
-
 </script>
 
-<main class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-	{$queryResult.data}
-	<div class="mt-10 sm:mt-0">
-		<div class="overflow-hidden shadow sm:rounded-md">
-			<div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
-				<nav class="flex items-center justify-between border-b border-gray-200 px-4 sm:px-0">
-					<PreviousButton {currentQuestionIndex} {previousSlide} />
+{$queryResult.data}
+<div class="mt-10 sm:mt-0">
+	<div class="overflow-hidden shadow sm:rounded-md">
+		<div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+			<nav class="flex items-center justify-between border-b border-gray-200 px-4 sm:px-0">
+				<PreviousButton {currentQuestionIndex} {previousSlide} />
 
-					<div class="hidden md:-mb-px md:flex">
-						{#each questions as _question, index}
-							<button
-								class={`${
-									index === currentQuestionIndex ? 'border-rose-500 text-rose-600' : ''
-								} inline-flex items-center border-b-2 border-transparent px-4 py-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700`}
-								on:click={() => jumpSlide(index)}
-							>
-								{index + 1}
-							</button>
-						{/each}
-					</div>
-					<NextButton {currentQuestionIndex} {nextSlide} />
-				</nav>
-			</div>
-			<div class="space-y-6 bg-white px-4 py-5 sm:p-6">
-				<FormGroup {question} />
-				<FormDivider />
-			</div>
+				<div class="hidden md:-mb-px md:flex">
+					{#each questions as _question, index}
+						<button
+							class={`${
+								index === currentQuestionIndex ? 'border-rose-500 text-rose-600' : ''
+							} inline-flex items-center border-b-2 border-transparent px-4 py-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700`}
+							on:click={() => jumpSlide(index)}
+						>
+							{index + 1}
+						</button>
+					{/each}
+				</div>
+				<NextButton {currentQuestionIndex} {nextSlide} />
+			</nav>
+		</div>
+		<div class="space-y-6 bg-white px-4 py-5 sm:p-6">
+			<FormGroup {question} />
+			<FormDivider />
 		</div>
 	</div>
-</main>
+</div>
