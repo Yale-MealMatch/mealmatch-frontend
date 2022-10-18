88824e41aabe67854d3c30f959d2ecdf7902b274
Braden Wong <bmw02002turbo@gmail.com>
2022-10-17 23:36:56 -0400

    Feat: Logo replaces img

---
+++ src/lib/Logo.svelte
@@ -0,0 +1,4 @@
+<script lang="ts">
+</script> 
+
+<img src="/logo.jpg" alt="MealMatch" class={$$props.class || ''}/>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Logo from '$lib/Logo.svelte';
 	import AuthCard from './AuthCard.svelte';
 </script>
 
@@ -13,11 +14,7 @@
 <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
 	<div class="w-full max-w-md space-y-8">
 		<div>
-			<img
-				class="mx-auto h-12 w-auto"
-				src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-				alt="Your Company"
-			/>
+			<Logo class="mx-auto h-12 w-auto" />
 			<h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
 				Sign in to your account
 			</h2>
