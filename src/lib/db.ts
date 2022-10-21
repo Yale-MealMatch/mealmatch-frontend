000f469f767879988f1ba679df031ba2761c5683
Braden Wong <bmw02002turbo@gmail.com>
2022-10-21 00:37:40 -0400

    Refactor: db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,3 +1,4 @@
+	// From https://github.com/supabase/auth-helpers/blob/main/packages/sveltekit/README.md 
 import { createClient } from '@supabase/supabase-js';
 import { env } from '$env/dynamic/public'
 
