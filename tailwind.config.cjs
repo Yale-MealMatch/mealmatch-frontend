48a9605b24e6aacbb5032ca4136ff47cfb71a83c
Braden Wong <bmw02002turbo@gmail.com>
2022-10-26 20:30:20 -0400

    Feat: Open Sans Text

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				sans: ['Open Sans', ...defaultTheme.fontFamily.sans]
+			}
+		}
 	},
 
 	plugins: [require('@tailwindcss/forms')]
