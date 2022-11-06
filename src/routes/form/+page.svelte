5be84c5c6d42c5a068dfd9018cf5b8d151f8110e
Braden Wong <bmw02002turbo@gmail.com>
2022-11-06 16:10:51 -0500

    Feat: Working Progress Nav Bar

--- src/routes/form/+page.svelte
+++ src/routes/form/+page.svelte
@@ -1,10 +1,12 @@
-<script>
+<script lang="ts">
 	import FormCheckboxes from '$lib/form/FormCheckboxes.svelte';
 	import FormDivider from '$lib/form/FormDivider.svelte';
 	import FormHeader from '$lib/form/FormHeader.svelte';
 	import FormInput from '$lib/form/FormInput.svelte';
 	import FormRadioGroup from '$lib/form/FormRadioGroup.svelte';
 	import FormTextArea from '$lib/form/FormTextArea.svelte';
+	import NextButton from '$lib/form/NextButton.svelte';
+	import PreviousButton from '$lib/form/PreviousButton.svelte';
 	import { questions } from '$lib/form/questions';
 	import ProgressBar from './ProgressBar.svelte';
 
@@ -14,20 +16,19 @@
 	let isError = false;
 	const previousSlide = () => (isError ? '' : currentPageIndex--);
 	const nextSlide = () => (isError ? '' : currentPageIndex++);
-	// const jumpSlide = (index: number) => (isError ? '' : (currentPageIndex = index));
+	const jumpSlide = (index: number) => (isError ? '' : (currentPageIndex = index));
 </script>
 
 <div class="mt-10 sm:mt-0">
 	<div class="overflow-hidden shadow sm:rounded-md">
 		<!-- svelte-ignore missing-declaration -->
 		<div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
-			<!-- <nav class="flex items-center justify-between border-b border-gray-200 px-4 sm:px-0">
-				<PreviousButton {currentQuestionIndex} {previousSlide} />
-
+			<nav class="flex items-center justify-between border-b border-gray-200 px-4 sm:px-0">
+				<PreviousButton currentIndex={currentPageIndex} {previousSlide} />
 				<div class="hidden md:-mb-px md:flex">
 					{#each questions as _question, index}
 						<button
-							class="{index === currentQuestionIndex
+							class="{index === currentPageIndex
 								? 'border-rose-500 text-rose-600'
 								: ''} inline-flex items-center border-b-2 border-transparent px-4 py-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
 							on:click={() => jumpSlide(index)}
@@ -36,8 +37,8 @@
 						</button>
 					{/each}
 				</div>
-				<NextButton {currentQuestionIndex} {nextSlide} />
-			</nav> -->
+				<NextButton currentIndex={currentPageIndex} {nextSlide} />
+			</nav>
 			<ProgressBar />
 		</div>
 		<div class="space-y-6 bg-white px-4 py-5 sm:p-6">
