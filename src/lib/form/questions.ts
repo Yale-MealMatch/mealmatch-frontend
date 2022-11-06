0f683c021900f31ec5ac151ed8087cfa4e7a5d63
Braden Wong <bmw02002turbo@gmail.com>
2022-11-06 15:52:43 -0500

    Feat: New Questions

--- src/lib/form/questions.ts
+++ src/lib/form/questions.ts
@@ -1,85 +1,110 @@
 import type { Question } from './types';
 
-export const questions: Question[] = [
-	{
-		type: 'radio',
-		name: 'year',
-		label: 'What year are you?',
-		description: '',
-		options: [
-			{ label: 'First-year', value: 2026 },
-			{ label: 'Sophomore', value: 2025 },
-			{ label: 'Junior', value: 2024 },
-			{ label: 'Senior', value: 2023 }
-		],
-		validationFunction: (value) => value > 0,
-		errorMessage: 'Please select a year'
-	},
-	{
-		type: 'checkboxes',
-		name: 'year_match',
-		label: 'What year(s) would you like to match with?',
-		description: 'We will try to match you with someone within your preferences.',
-		options: [
-			{ label: 'First-year', value: 2026 },
-			{ label: 'Sophomore', value: 2025 },
-			{ label: 'Junior', value: 2024 },
-			{ label: 'Senior', value: 2023 }
-		],
-		validationFunction: (value) => value.length > 0,
-		errorMessage: 'Please select at least one year'
-	},
-	{
-		type: 'radio',
-		name: 'pronouns',
-		label: 'What are your pronouns?',
-		description: 'Your pronouns will be provided to your match for ease of communication.',
-		options: [
-			{ label: 'he/him', value: 'he/him' },
-			{ label: 'she/her', value: 'she/her' },
-			{ label: 'they/them', value: 'they/them' },
-			{ label: 'other', value: 'other' }
-		],
-		validationFunction: (value) => value !== '',
-		errorMessage: 'Please select your pronouns'
-	},
-	{
-		type: 'radio',
-		name: 'pronouns_match',
-		label: 'Match me with someone with ___ pronouns.',
-		description: 'We will try our best!',
-		options: [
-			{ label: 'Any', value: 'any' },
-			{ label: 'Same', value: 'same' },
-			{ label: 'Different', value: 'different' }
-		],
-		validationFunction: (value) => value !== '',
-		errorMessage: 'Please select at least one pronoun'
-	},
-	{
-		type: 'input',
-		name: 'phone',
-		label: 'What is your phone number? (Ten digits, no special characters)',
-		description: 'Your number will be provided to your match for ease of communication.',
-		placeholder: '1234567890',
-		validationFunction: (value) => /^\d{10}$/.test(value),
-		errorMessage: 'Please enter 10 digits in the form 1234567890'
-	},
-	{
-		type: 'input',
-		name: 'nickname',
-		label: 'Create a nickname:',
-		description:
-			"Your match will refer to you by your nickname before meeting in person. Don't use your real name. ",
-		placeholder: 'Your answer',
-		validationFunction: (value) => value.length > 0,
-		errorMessage: 'Please enter a nickname.'
-	},
-	{
-		type: 'textarea',
-		name: 'bio',
-		label: 'Personal description (75 words max, optional)',
-		description: `<p>
+export const questions: Question[][] = [
+	[
+		{
+			type: 'input',
+			name: 'email',
+			label: 'What is email address? (Yale)',
+			description: 'Please make sure that this is correct',
+			placeholder: 'jane.doe@yale.edu',
+			validationFunction: (value) => /@yale.edu$/.test(value),
+			errorMessage: 'Please enter 10 digits in the form 1234567890'
+		},
+		{
+			type: 'input',
+			name: 'phone',
+			label: 'What is your phone number? (Ten digits, no special characters)',
+			description: 'Your number will be provided to your match for ease of communication.',
+			placeholder: '1234567890',
+			validationFunction: (value) => /^\d{10}$/.test(value),
+			errorMessage: 'Please enter 10 digits in the form 1234567890'
+		},
+		{
+			type: 'radio',
+			name: 'year',
+			label: 'What year are you?',
+			description: '',
+			options: [
+				{ label: 'First-year', value: 2026 },
+				{ label: 'Sophomore', value: 2025 },
+				{ label: 'Junior', value: 2024 },
+				{ label: 'Senior', value: 2023 }
+			],
+			validationFunction: (value) => value > 0,
+			errorMessage: 'Please select a year'
+		},
+		{
+			type: 'radio',
+			name: 'pronouns',
+			label: 'What are your pronouns?',
+			description: 'Your pronouns will be provided to your match for ease of communication.',
+			options: [
+				{ label: 'he/him', value: 'he/him' },
+				{ label: 'she/her', value: 'she/her' },
+				{ label: 'they/them', value: 'they/them' },
+				{ label: 'other', value: 'other' }
+			],
+			validationFunction: (value) => value !== '',
+			errorMessage: 'Please select your pronouns'
+		},
+		{
+			type: 'input',
+			name: 'nickname',
+			label: 'Create a nickname:',
+			description:
+				"Your match will refer to you by your nickname before meeting in person. Don't use your real name. ",
+			placeholder: 'Your answer',
+			validationFunction: (value) => value.length > 0,
+			errorMessage: 'Please enter a nickname.'
+		}
+	],
+	[
+		{
+			type: 'checkboxes',
+			name: 'year_match',
+			label: 'What year(s) would you like to match with?',
+			description: 'We will try to match you with someone within your preferences.',
+			options: [
+				{ label: 'First-year', value: 2026 },
+				{ label: 'Sophomore', value: 2025 },
+				{ label: 'Junior', value: 2024 },
+				{ label: 'Senior', value: 2023 }
+			],
+			validationFunction: (value) => value.length > 0,
+			errorMessage: 'Please select at least one year'
+		},
+		{
+			type: 'radio',
+			name: 'pronouns_match',
+			label: 'Match me with someone with ___ pronouns.',
+			description: 'We will try our best!',
+			options: [
+				{ label: 'Any', value: 'any' },
+				{ label: 'Same', value: 'same' },
+				{ label: 'Different', value: 'different' }
+			],
+			validationFunction: (value) => value !== '',
+			errorMessage: 'Please select an option'
+		},
+		{
+			type: 'radio',
+			name: 'pronouns_match',
+			label: 'Match me with someone with...',
+			description: 'We will try our best!',
+			options: [
+				{ label: 'Similar keywords as me', value: 'any' },
+				{ label: 'Different keywords as me', value: 'same' },
+				{ label: 'With these specific keywords', value: 'different' }
+			],
+			validationFunction: (value) => value !== '',
+			errorMessage: 'Please select an option'
+		},
+		{
+			type: 'textarea',
+			name: 'bio',
+			label: 'Personal description (75 words max, optional)',
+			description: `<p>
 		Based on your answer to this question, we will try to match you with someone you are compatible
 		with. If you choose not to answer it, we will match you randomly.
 			</p>
@@ -94,8 +119,9 @@ export const questions: Question[] = [
 		<p>Please don't include any information that directly reveals who you are.</p>
 		</div>
 		`,
-		placeholder: 'I like to be',
-		validationFunction: () => true,
-		errorMessage: ''
-	}
+			placeholder: 'I like to be',
+			validationFunction: () => true,
+			errorMessage: ''
+		}
+	]
 ];
