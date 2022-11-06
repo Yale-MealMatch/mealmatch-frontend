8504929901e2283f6a79d7833483e1a82df42a0d
Braden Wong <bmw02002turbo@gmail.com>
2022-11-06 16:11:46 -0500

    Feat: Move pagination down to bottom

--- src/routes/form/+page.svelte
+++ src/routes/form/+page.svelte
@@ -23,22 +23,6 @@
 	<div class="overflow-hidden shadow sm:rounded-md">
 		<!-- svelte-ignore missing-declaration -->
 		<div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
-			<nav class="flex items-center justify-between border-b border-gray-200 px-4 sm:px-0">
-				<PreviousButton currentIndex={currentPageIndex} {previousSlide} />
-				<div class="hidden md:-mb-px md:flex">
-					{#each questions as _question, index}
-						<button
-							class="{index === currentPageIndex
-								? 'border-rose-500 text-rose-600'
-								: ''} inline-flex items-center border-b-2 border-transparent px-4 py-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
-							on:click={() => jumpSlide(index)}
-						>
-							{index + 1}
-						</button>
-					{/each}
-				</div>
-				<NextButton currentIndex={currentPageIndex} {nextSlide} />
-			</nav>
 			<ProgressBar />
 		</div>
 		<div class="space-y-6 bg-white px-4 py-5 sm:p-6">
@@ -60,6 +44,24 @@
 				</div>
 				<FormDivider />
 			{/each}
+		</div>
+		<div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+			<nav class="flex items-center justify-between border-b border-gray-200 px-4 sm:px-0">
+				<PreviousButton currentIndex={currentPageIndex} {previousSlide} />
+				<div class="hidden md:-mb-px md:flex">
+					{#each questions as _question, index}
+						<button
+							class="{index === currentPageIndex
+								? 'border-rose-500 text-rose-600'
+								: ''} inline-flex items-center border-b-2 border-transparent px-4 py-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+							on:click={() => jumpSlide(index)}
+						>
+							{index + 1}
+						</button>
+					{/each}
+				</div>
+				<NextButton currentIndex={currentPageIndex} {nextSlide} />
+			</nav>
 		</div>
 	</div>
 </div>
