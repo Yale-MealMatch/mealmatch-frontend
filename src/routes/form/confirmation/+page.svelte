b83ddfed8adc7d5c30da0ba17d15c260542ba151
Braden Wong <bmw02002turbo@gmail.com>
2022-11-10 23:31:03 -0500

    Feat: Confirmation Page

---
+++ src/routes/form/confirmation/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Logo from '$lib/Logo.svelte';
+</script>
+
+<div class="mt-10 sm:my-4">
+	<div class="flex flex-col gap-4">
+		<div class="rounded-lg border border-gray-200 bg-white p-6 shadow-md">
+			Thank you so much for your submission!
+		</div>
+		<Logo />
+	</div>
+</div>
