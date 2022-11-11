f31101c4b3baa3d1cc1afd26e3158240f3f7a125
Braden Wong <bmw02002turbo@gmail.com>
2022-11-10 23:41:01 -0500

    Feat: Paragraphs

--- src/routes/form/confirmation/+page.svelte
+++ src/routes/form/confirmation/+page.svelte
@@ -5,7 +5,9 @@
 <div class="mt-10 sm:my-4">
 	<div class="flex flex-col gap-4">
 		<div class="rounded-lg border border-gray-200 bg-white p-6 shadow-md">
-			Thank you so much for your submission!
+			<p>Thank you for using MealMatch!</p>
+			<p>We'll send a confirmation text later this week—please remember to reply.</p>
+			<p>Hope you enjoy your meal!</p>
 		</div>
 		<Logo />
 	</div>
