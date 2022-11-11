739ec40680784e9d4fd88990ffa3e463c552beaf
Braden Wong <bmw02002turbo@gmail.com>
2022-11-10 23:42:15 -0500

    Feat: Confirmation Message

--- src/routes/form/confirmation/+page.svelte
+++ src/routes/form/confirmation/+page.svelte
@@ -4,9 +4,9 @@
 
 <div class="mt-10 sm:my-4">
 	<div class="flex flex-col gap-4">
-		<div class="rounded-lg border border-gray-200 bg-white p-6 shadow-md">
+		<div class="rounded-lg border border-gray-200 bg-white p-6 shadow-md space-y-4">
 			<p>Thank you for using MealMatch!</p>
-			<p>We'll send a confirmation text later this week—please remember to reply.</p>
+			<p>We'll send a confirmation text later this week—please remember to reply :)</p>
 			<p>Hope you enjoy your meal!</p>
 		</div>
 		<Logo />
