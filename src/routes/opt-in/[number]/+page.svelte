8efc0b5f2fcc8a8604466291066cf2c19bc7b29c
Braden Wong <bmw02002turbo@gmail.com>
2022-11-14 16:24:19 -0500

    Feat: Opt In Page

---
+++ src/routes/opt-in/[number]/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	// Grab the params
+	import { page } from '$app/stores';
+	import { supabaseClient } from '$lib/db';
+
+	let optIn = supabaseClient.from('opt_ins').insert({
+		phone: $page.params.number,
+		week: 1
+	});
+</script>
+
+<!-- Await optIn -->
+{#await optIn}
+	<!-- Loading -->
+	<div>Loading, please wait...</div>
+{:then data}
+	{#if data.error}
+		<!-- Error -->
+		<div>{data.error}</div>
+	{:else}
+		<!-- Success -->
+		<div>Success! You are opted into matching for this week</div>
+	{/if}
+{:catch error}
+	<!-- Error -->
+	<div>Something went wrong</div>
+{/await}
