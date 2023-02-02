1abe5fa9d45d1084248a2ab77c9233eefe04282b
Braden Wong <bmw02002turbo@gmail.com>
2023-02-01 21:57:29 -0500

    fix: shorten hook

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,20 +1,8 @@
 // From https://github.com/supabase/auth-helpers/blob/main/packages/sveltekit/README.md
 import '$lib/db';
-import { getSupabase } from '@supabase/auth-helpers-sveltekit';
-import { redirect, type Handle } from '@sveltejs/kit';
+import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	const { session } = await getSupabase(event);
-
-	if (!session) {
-		throw redirect(303, '/login');
-	}
-
-	// Now we are authenticated
-	if (event.url.pathname.startsWith('/login')) {
-		throw redirect(303, '/form');
-	}
-
 	const response = await resolve(event);
 
 	return response;
