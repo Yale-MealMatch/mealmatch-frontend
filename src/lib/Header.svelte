1ce2085e6eb20f7cd0a5b29d5567f3e67806e148
Braden Wong <bmw02002turbo@gmail.com>
2023-02-05 16:34:02 -0500

    chore: layout

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -76,9 +76,8 @@
 						/>
 					</div>
 				</div>
-			</div>
+			</div> -->
 			<div class="flex lg:hidden">
-				<!- - Mobile menu button - ->
 				<DisclosureButton
 					class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-neutral-800 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
 				>
@@ -92,15 +91,14 @@
 			</div>
 			<div class="hidden lg:ml-4 lg:block">
 				<div class="flex items-center">
-					<button
+					<!-- <button
 						type="button"
 						class="flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
 					>
 						<span class="sr-only">View notifications</span>
 						<Icon src={BellAlert} theme="solid" class="h-6 w-6" aria-hidden="true" />
-					</button>
+					</button> -->
 
-					<!- - Profile dropdown - ->
 					<Menu as="div" class="relative ml-4 flex-shrink-0">
 						<div>
 							<MenuButton
@@ -127,18 +125,18 @@
 							>
 								<MenuItem let:active>
 									<a
-										href="#"
+										href="/form"
 										class:bg-gray-100={active}
 										class="block px-4 py-2 text-sm text-gray-700">Your Profile</a
 									>
 								</MenuItem>
-								<MenuItem let:active>
+								<!-- <MenuItem let:active>
 									<a
 										href="#"
 										class:bg-gray-100={active}
 										class="block px-4 py-2 text-sm text-gray-700">Settings</a
 									>
-								</MenuItem>
+								</MenuItem> -->
 								<MenuItem let:active>
 									<button
 										on:click={signOut}
@@ -186,7 +184,7 @@
 				>
 					<span class="sr-only">View notifications</span>
 					<Icon src={BellAlert} class="h-6 w-6" aria-hidden="true" />
-					<!- - <BellIcon class="h-6 w-6" aria-hidden="true" /> - ->
+					<!-- <BellIcon class="h-6 w-6" aria-hidden="true" /> -->
 				</button>
 			</div>
 			<div class="mt-3 space-y-1 px-2">
@@ -211,7 +209,4 @@
 			</div>
 		</div>
 	</DisclosurePanel>
-	 -->
-		</div>
-	</div></Disclosure
->
+</Disclosure>
