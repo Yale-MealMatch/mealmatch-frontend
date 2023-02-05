47c7d6bea0b083170d8cc1a3759348d28e5d6c6e
Braden Wong <bmw02002turbo@gmail.com>
2023-02-05 16:09:35 -0500

    fix: types

--- src/lib/form/types.d.ts
+++ src/lib/form/types.d.ts
@@ -1,36 +1,36 @@
 import type { Database } from 'types/supabase';
+
+export type Row = Database['public']['Tables']['profiles']['Row'];
+
 type SelectOption = {
 	label: string;
 	value: string;
 	subtitle?: string;
 };
 
-type FormCheckboxesOption = SelectOption;
-type FormRadioSelectOption = SelectOption;
-
 type FormRadio = {
 	type: 'radio';
-	name: keyof Database['public']['Tables']['profiles']['Row'];
+	name: keyof Row;
 	label: string;
 	description: string;
-	options: FormRadioSelectOption[];
+	options: SelectOption[];
 	validationFunction: (value: string) => boolean;
 	errorMessage: string;
 };
 
 type FormCheckboxes = {
 	type: 'checkboxes';
-	name: keyof Database['public']['Tables']['profiles']['Row'];
+	name: keyof Row;
 	label: string;
 	description: string;
-	options: FormCheckboxesOption[];
+	options: SelectOption[];
 	validationFunction: (value: string[]) => boolean;
 	errorMessage: string;
 };
 
 type FormInput = {
 	type: 'input';
-	name: keyof Database['public']['Tables']['profiles']['Row'];
+	name: keyof Row;
 	label: string;
 	description: string;
 	placeholder: string;
@@ -41,7 +41,7 @@ type FormInput = {
 // Description can be a string or a Svelte component
 type FormTextArea = {
 	type: 'textarea';
-	name: keyof Database['public']['Tables']['profiles']['Row'];
+	name: keyof Row;
 	label: string;
 	description: string | typeof SvelteComponent;
 	placeholder: string;
