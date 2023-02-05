9e72528e01d7fc060b097178335e6a932a8211ca
Braden Wong <bmw02002turbo@gmail.com>
2023-02-05 15:41:07 -0500

    feat: modify questions to fit types

--- src/lib/form/questions.ts
+++ src/lib/form/questions.ts
@@ -8,12 +8,12 @@ export const questions: Question[][] = [
 			label: 'What year are you?',
 			description: '',
 			options: [
-				{ label: 'First-year', value: 2026 },
-				{ label: 'Sophomore', value: 2025 },
-				{ label: 'Junior', value: 2024 },
-				{ label: 'Senior', value: 2023 }
+				{ label: 'First-year', value: '2026' },
+				{ label: 'Sophomore', value: '2025' },
+				{ label: 'Junior', value: '2024' },
+				{ label: 'Senior', value: '2023' }
 			],
-			validationFunction: (value) => value > 0,
+			validationFunction: (value) => !!value,
 			errorMessage: 'Please select a year'
 		},
 		{
@@ -28,7 +28,7 @@ export const questions: Question[][] = [
 				{ label: 'other', value: 'other' }
 				// { label: 'prefer not to say', value: 'prefer not to say' }
 			],
-			validationFunction: (value) => value !== '',
+			validationFunction: (value) => !!value,
 			errorMessage: 'Please select your pronouns'
 		}
 	],
@@ -39,12 +39,12 @@ export const questions: Question[][] = [
 			label: 'What year(s) would you like to match with?',
 			description: 'We will try to match you with someone within your preferences.',
 			options: [
-				{ label: 'First-year', value: 2026 },
-				{ label: 'Sophomore', value: 2025 },
-				{ label: 'Junior', value: 2024 },
-				{ label: 'Senior', value: 2023 }
+				{ label: 'First-year', value: '2026' },
+				{ label: 'Sophomore', value: '2025' },
+				{ label: 'Junior', value: '2024' },
+				{ label: 'Senior', value: '2023' }
 			],
-			validationFunction: (value) => value.length > 0,
+			validationFunction: (value) => !!value,
 			errorMessage: 'Please select at least one year'
 		},
 		{
@@ -57,7 +57,7 @@ export const questions: Question[][] = [
 				{ label: 'same', value: 'same' },
 				{ label: 'different', value: 'different' }
 			],
-			validationFunction: (value) => value !== '',
+			validationFunction: (value) => !!value,
 			errorMessage: 'Please select an option'
 		}
 	],
@@ -87,13 +87,13 @@ export const questions: Question[][] = [
 			description:
 				'For privacy, your match will refer to you by your nickname before meeting in person.',
 			placeholder: 'Your nickname',
-			validationFunction: (value) => value.length > 0,
+			validationFunction: (value) => !!value,
 			errorMessage: 'Please enter a nickname'
 		},
 		{
 			type: 'textarea',
 			name: 'bio',
-			label: '<span class="text-slate-500">(Recommended)</span> Personal description',
+			label: '<span class="text-slate-500">(Recommended)</span> Personal Bio',
 			// description:
 			description: `Based on your answer to this question, we will try to match you with someone you are compatible with. If you choose not to answer it, we will match you randomly.
 <br />
@@ -116,8 +116,8 @@ Please don't include any information that directly reveals who you are.`,
 			placeholder: 'List keywords separated by commas',
 			// validationFunction: (value) => /,\s*/.test(value),
 			// eslint-disable-next-line @typescript-eslint/no-unused-vars
-			validationFunction: (value) => true,
-			errorMessage: 'Please enter a comma separated list of keywords'
+			validationFunction: (value) => !!value,
+			errorMessage: 'Please enter a bio'
 		},
 		// {
 		// 	type: 'input',
@@ -141,33 +141,33 @@ Please don't include any information that directly reveals who you are.`,
 			// description:
 			// 'For instance, "literature, philosophy, Led Zeppelin, entrepreneurship, swimming, curious"',
 			description:
-				'They can be the same as yours, or different. We will try our best to match you to someone with these keywords!',
+				'They can be the same as yours, or different. We will try our best to match you to someone with these keywords in their description!',
 			placeholder: 'List keywords separated by commas',
 			// validationFunction: (value) => /,\s*/.test(value),
 			// eslint-disable-next-line @typescript-eslint/no-unused-vars
-			validationFunction: (value) => true,
+			validationFunction: () => true,
 			errorMessage: 'Please enter a comma separated list of keywords'
 		},
 		{
 			type: 'checkboxes',
-			name: 'confirm',
+			name: 'signed_confirmation',
 			label: 'By submitting this form, I agree to the following:',
 			description: '',
 			options: [
 				{
 					label:
 						'I will not ghost my match, and understand that doing so may ban me from using MealMatch in the future.',
-					value: 1
+					value: '1'
 				},
 				{
 					label:
 						'I will not ask for any identity-revealing information (real name, Instagram, etc.) from my match before meeting them in person unless they volunteer to do so.',
-					value: 2
+					value: '2'
 				},
 				{
 					label:
 						'I certify that my email and phone number are correct, and that I will opt in via weekly confirmation texts to be matched.',
-					value: 3
+					value: '3'
 				}
 			],
 			validationFunction: (value) => value.length === 3,
