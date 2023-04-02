56797868bea1e4c918b56058f7d65790a3cd5ba2
Braden Wong <bmw02002turbo@gmail.com>
2023-04-02 16:19:28 -0400

    feat: use hooks.server.ts instead of page loads for auth guards

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,14 @@
 // From https://github.com/supabase/auth-helpers/blob/main/packages/sveltekit/README.md
 import '$lib/supabase';
-import type { Handle } from '@sveltejs/kit';
+import { getSupabase } from '@supabase/auth-helpers-sveltekit';
+import { redirect, type Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
+	const { session } = await getSupabase(event);
+	const { url } = event;
+	if (!session && url.pathname !== '/') {
+		throw redirect(303, '/');
+	}
 	const response = await resolve(event);
 
 	return response;
--- src/routes/(app)/form/+page.ts
+++ src/routes/(app)/form/+page.ts
@@ -1,14 +1,9 @@
 import { getSupabase } from '@supabase/auth-helpers-sveltekit';
 import type { PageLoad } from './$types';
-import { redirect } from '@sveltejs/kit';
 import { getUserResponses, responses } from '$lib/stores/userResponses';
 
 export const load: PageLoad = async (event) => {
 	const { session } = await getSupabase(event);
-	if (!session?.user.email) {
-		throw redirect(303, '/login');
-	}
 	const userResponses = await getUserResponses(session.user.email);
-	console.log('🚀 ~ file: +page.ts:12 ~ constload:PageLoad= ~ userResponses:', userResponses);
 	responses.set(userResponses);
 };
