f0f15b51d11059f5e350f9896536b345294b45c7
lcaixv <kai.xu@yale.edu>
2023-04-02 16:47:49 -0400

    changed text in landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -32,54 +32,30 @@
 			class:bg-opacity-40={hover}
 			class:bg-opacity-50={!hover}
 		>
-			<div class="flex flex-col gap-4 p-8 text-white">
-				<h1 class="max-w-3xl text-3xl tracking-wide sm:text-5xl ">
-					Which Residential College is <span class="tracking-widest">Actually</span> the Best?
-				</h1>
-				<h2 class="sm:text-2xl ">
-					Solve the Most Question Important on Campus<span
-						class="bg-gradient-to-l from-current to-white bg-clip-text text-transparent">...</span
-					>
-				</h2>
-				<div class="flex gap-2 sm:gap-2">
-					<button
-						on:click={signInWithGoogle}
-						class="inline-flex w-fit justify-center gap-1 rounded-lg bg-stone-200 py-3 px-6 text-stone-700 shadow-xl hover:shadow-2xl"
-						on:mouseover={() => (hover = true)}
-						on:mouseout={() => (hover = false)}
-						on:focus={() => (hover = true)}
-						on:blur={() => (hover = false)}
-					>
-						<!-- <GoogleIcon /> -->
-						<span class="tracking-wider">
-							{loggedIn ? 'Vote and find out' : 'Sign in with yale.edu to find out'}
-						</span>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="1.5"
-							stroke="currentColor"
-							class="h-6 w-6"
-						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z"
-							/>
-						</svg>
-					</button>
-					<button
-						class="inline-flex w-fit items-center justify-center rounded-lg bg-stone-700 px-2 shadow-xl hover:shadow-2xl"
-						on:click={() => {
-							backgroundDiningHallIndex =
-								(backgroundDiningHallIndex + 1) % NUMBER_BACKGROUND_IMAGES;
-						}}
-					>
-						<PlaceholderImage />
-					</button>
-				</div>
-			</div>
+		<div class="flex flex-col gap-4 p-8 text-white">
+  <h1 class="max-w-3xl text-3xl tracking-wide">MealMatch</h1>
+  <h2 class="sm:text-4xl">Meet Yalies who you're more likely to vibe with based on who you are.</h2>
+  <div class="flex gap-2 sm:gap-2">
+    <button
+      on:click={signInWithGoogle}
+      class="inline-flex items-center justify-center gap-1 w-fit py-3 px-6 rounded-lg bg-stone-200 text-stone-700 shadow-xl hover:shadow-2xl"
+      on:mouseover={() => (hover = true)}
+      on:mouseout={() => (hover = false)}
+      on:focus={() => (hover = true)}
+      on:blur={() => (hover = false)}
+    >
+      <span class="tracking-wider text-lg">{loggedIn ? 'Get Started' : 'Get Started'}</span>
+    </button>
+    <button
+      class="inline-flex items-center justify-center w-fit px-2 rounded-lg bg-stone-700 shadow-xl hover:shadow-2xl"
+      on:click={() => {
+        backgroundDiningHallIndex = (backgroundDiningHallIndex + 1) % NUMBER_BACKGROUND_IMAGES;
+      }}
+    >
+      <PlaceholderImage />
+    </button>
+  </div>
+</div>
 		</div>
 	</div>
 </div>
